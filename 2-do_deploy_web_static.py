8d84b4ae3b0a2c2d6f07d3b08c387d0cb9330358
Williams <akanniwilliams@gmail.com>
2023-10-12 16:40:44 +0100

    Deploy web_static onto webservers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,13 +3,13 @@
 Deploy archive to web servers
 """
 from fabric.api import *
-from fabric.decorators import task
 import os
 from datetime import datetime
 
-
-#env.hosts = ['100.25.147.62', '54.174.169.156']
-env.hosts= ["c8f1563fc557.49c6c158.alx-cod.online"]
+env.hosts = ["100.25.147.62", "54.174.169.156"]
+env.user = "ubuntu"
+# env.hosts = ['172.17.0.2']
+# env.user = "root"
 
 
 @task
@@ -24,10 +24,11 @@ def do_deploy(archive_path):
             dst = "/data/web_static/releases/{}".format(file_name)
             run("mkdir -p " + dst + "/")
             run("tar -xzf /tmp/{}.{} -C {}/".format(file_name, ext, dst))
-            run("rm /tmp/{}.{}".formart(file_name, ext))
+            run("rm /tmp/{}.{}".format(file_name, ext))
             run("mv {}/web_static/* {}/".format(dst, dst))
             run("rm -rf {}/web_static".format(dst))
-            run("ln -rf {}.{} /data/web_static/current".format(dst, ext))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {}/ /data/web_static/current".format(dst))
             return True
         else:
             return False
