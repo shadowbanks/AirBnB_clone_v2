79b64be5b4e58c0803374a58935d531e77905d92
Williams <akanniwilliams@gmail.com>
2023-10-21 04:51:14 +0100

    More routes (check if <n> is a number)

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+First Flask ^^
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def main():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return f"C {escape(text)}".replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_is_fun(text="is cool"):
+    return f"Python {escape(text)}".replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_number(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
